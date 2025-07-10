1166f1e3f702522ee09b45304c44da8a4efaf65a
armen-asriyan <armenasriyan@proton.me>
2025-07-10 20:47:24 +0200

    fixed no unused vars

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer({
-  activeSection,
   setActiveSection,
 }: {
-  activeSection: string | null;
   setActiveSection: (section: string) => void;
 }) {
   const handleClick = (section: string) => {
