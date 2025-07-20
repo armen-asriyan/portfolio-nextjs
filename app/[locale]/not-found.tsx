7041397c7ffbebeaf7f88e380b656bd745c7f8f4
armen-asriyan <armenasriyan@proton.me>
2025-07-20 15:54:29 +0200

    better favicon handling, dynamic sitemap generation instead of a static sitemap file, translated the not-found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,6 +1,9 @@
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("notFound");
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
       <pre
@@ -8,19 +11,19 @@ export default function NotFound() {
         style={{ fontFamily: "Roboto" }}
       >
         {`A_A
-                (╹ -╹)   - huh ?`}
+                (╹ -╹)   - ${t("huh")} ?`}
       </pre>
       <h1 className="text-6xl font-bold mb-4">404</h1>
 
       <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">
-        Page not found.
+        {t("description")}
       </p>
 
       <Link
         href="/"
         className="px-6 py-2 text-white bg-purple-700 hover:bg-purple-800 rounded-md transition"
       >
-        Go back home
+        {t("link")}
       </Link>
     </div>
   );
