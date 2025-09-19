419d1a36fa34e327b5a215714f3af03413f135dd
armen-asriyan <armenasriyan@proton.me>
2025-09-19 15:42:42 +0200

    added names for the skills on hover

--- components/SkillIcon.tsx
+++ components/SkillIcon.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { ComponentType } from "react";
+import { ComponentType, useEffect, useRef, useState } from "react";
 
 type SkillIconProps = {
-  icon: ComponentType<{ className?: string }>;
+  icon: ComponentType<{ className?: string; ref?: React.Ref<SVGSVGElement> }>;
   hoverColor?: string;
   darkIcon?: boolean;
 };
@@ -19,6 +19,16 @@ export default function SkillIcon({
     resolvedTheme,
   } = useTheme();
 
+  const [title, setTitle] = useState<string>("");
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      const svgTitle = svgRef.current.querySelector("title")?.textContent;
+      if (svgTitle) setTitle(svgTitle);
+    }
+  }, []);
+
   const isDarkTheme = resolvedTheme === "dark";
 
   const baseClasses =
@@ -26,7 +36,7 @@ export default function SkillIcon({
 
   return (
     <div
-      className={`rounded-md border-2 border-gray-300 dark:border-accent text-gray-900  dark:text-gray-300 cursor-pointer transition-all hover:shadow-[0_0_10px_#d1d5db]`}
+      className={`group rounded-md border-2 border-gray-300 dark:border-accent text-gray-900 dark:text-gray-300 cursor-pointer transition-all hover:shadow-[0_0_10px_#d1d5db] relative`}
       style={{
         color: isDarkTheme ? "#e5e7eb" : undefined,
         transition: "color 0.3s ease-in-out",
@@ -40,7 +50,15 @@ export default function SkillIcon({
           e.currentTarget.style.color = "";
       }}
     >
-      <Icon className={`${baseClasses}`} />
+      <p
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                   text-xs text-center z-50 pointer-events-none font-bold text-white 
+                   bg-black/50 rounded-md transition-all duration-300 ease-in-out 
+                   opacity-0 group-hover:opacity-100 p-1"
+      >
+        {title}
+      </p>
+      <Icon className={`${baseClasses}`} ref={svgRef} />
     </div>
   );
 }
